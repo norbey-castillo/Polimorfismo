abc5eb66ef087296d6955f213521f7444b0c71bf
Norbey <norbeycastillo890@gmail.com>
2026-04-23 10:03:36 -0600

    Ejercicio 1

---
+++ src/polimorfismo/Perro.java
@@ -0,0 +1,11 @@
+package polimorfismo;
+
+public class Perro extends Animal{
+
+	@Override
+	public void hacerSonido() {
+		System.out.println("El perro ladra! ");
+		
+	}
+
+}
---
+++ src/polimorfismo/main.java
@@ -0,0 +1,12 @@
+package polimorfismo;
+
+public class main {
+
+	public static void main(String[] args) {
+		Animal Perro = new Perro();
+		
+		Perro.hacerSonido();
+
+		
+	}
+}
