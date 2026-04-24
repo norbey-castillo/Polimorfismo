1ce6111aff56cd70ab5effaab9f29e019d0afe05
Norbey <norbeycastillo890@gmail.com>
2026-04-24 09:17:14 -0600

    verificando

--- src/polimorfismo/Rectangulo.java
+++ src/polimorfismo/Rectangulo.java
@@ -4,7 +4,7 @@ public class Rectangulo extends Figura{
 
 	@Override
 	public double area(double base, double altura) {
-		System.out.println("El area del rectangulo es: "+ base * altura);
+		System.out.println("\nEl area del rectangulo es: "+ base * altura);
 		return 0;
 		
 	}
